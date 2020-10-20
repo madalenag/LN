f004a209f8863cba937f0ddfe79d99d51c48d27a
taissahr <taissaribeiroebirdll@gmail.com>
2020-10-20 17:56:46 +0100

    70 for fine and 80 for coarse

--- qc.py
+++ qc.py
@@ -4,6 +4,12 @@ import string
 
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.metrics import accuracy_score
 
 f_labels=open("DEV-labels.txt", "w")
 f_questions=open("DEV-questions.txt", "w")
@@ -26,47 +32,86 @@ print ('Argument List:', str(sys.argv))
 
 arg_list = sys.argv
 
-f_train=open(arg_list[2], "r")
+f_train = open(arg_list[2], "r")
+f_dev_questions = open(arg_list[3], "r")
+f_dev_labels = open(arg_list[4], "r")
+
+model = arg_list[1]
 
 train_lines = f_train.readlines()
+dev_questions_lines = f_dev_questions.readlines()
+dev_labels_lines = f_dev_labels.readlines()
 
-train_dict = dict()
+train_questions = []
+train_labels = []
+
+dev_questions = []
+dev_labels = []
 
 for l in train_lines:
     split = l.split(' ',1)
-    if split[0] in train_dict:
-        # append the new number to the existing array at this slot
-        train_dict[split[0]].append(split[1])
+    split_label = split[0].split(':', 1)
+
+    train_questions.append(split[1])
+    if model == '-coarse':
+        train_labels.append(split_label[0])
     else:
-        # create a new array in this slot
-        train_dict[split[0]] = [split[1]]
+        train_labels.append(split[0])
 
-#print(train_dict['LOC:other'])
+for l in dev_questions_lines:
+    dev_questions.append(l)
 
+for l in dev_labels_lines:
+    split_label = l.split(':', 1)
+    if model == '-coarse':
+        dev_labels.append(split_label[0])
+    else:
+        dev_labels.append(l.replace('\n', ''))
+
+f_train.close()
+f_dev_questions.close()
+f_dev_labels.close()
 #pre processing
-
-#TODO: stopwords
-
-
+print(dev_labels)
 
 # TODO: stemming
+
+# Interface lemma tokenizer from nltk with sklearn
+class LemmaTokenizer:
+    def __init__(self):
+        self.wnl = WordNetLemmatizer()
+    def __call__(self, doc):
+        return [self.wnl.lemmatize(t) for t in word_tokenize(doc)]
 
 def process (question):
     question = question.lower()
     question = question.translate(str.maketrans('', '', string.punctuation))
-    question = question.rstrip()
+    question = question.replace('\n', '')
+    return question
 
-    stop_words = set(stopwords.words('english'))
-    exclude_words = set(("what", "where", "how", "when", "why"))
-    new_stop_words = stop_words.difference(exclude_words)
-    word_tokens = word_tokenize(question)
-    filtered_sentence = [w for w in word_tokens if not w in new_stop_words]
-    return filtered_sentence
+train_questions = list(map(process, train_questions))
+dev_questions = list(map(process, dev_questions))
 
-for key in train_dict:
-    train_dict[key] = list(map(process, train_dict[key]))
+stop_words = set(stopwords.words('english'))
+exclude_words = set(("what", "where", "how", "when", "why"))
+new_stop_words = stop_words.difference(exclude_words)
+
+tokenizer=LemmaTokenizer()
+token_stop = tokenizer(' '.join(new_stop_words))
+
+tfidfvectorizer = TfidfVectorizer(analyzer='word',stop_words= new_stop_words)#, tokenizer= tokenizer)
+
+train_data = tfidfvectorizer.fit_transform(train_questions)
+dev_data = tfidfvectorizer.transform(dev_questions)
+
+#train
+clf = ExtraTreesClassifier(n_estimators=100, random_state=0)
+clf.fit(train_data, train_labels)
+
+#predict
+dev_predict = clf.predict(dev_data)
 
 
-# TODO:  tokenization
+ #evaluating
 
-print(train_dict['LOC:other'])
+print(accuracy_score(dev_labels, dev_predict))
