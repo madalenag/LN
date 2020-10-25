8829fe1a1567110d68c12b783398fd5bc54bc680
taissahr <taissaribeiroebirdll@gmail.com>
2020-10-25 15:18:20 +0000

    final with report included

--- qc.py
+++ qc.py
@@ -8,46 +8,49 @@ from nltk.stem import WordNetLemmatizer
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.metrics import accuracy_score
+from sklearn import svm
 
-f_labels=open("DEV-labels.txt", "w")
-f_questions=open("DEV-questions.txt", "w")
+def split_dev(model):
+    f_labels=open("DEV-labels.txt", "w")
+    f_questions=open("DEV-questions.txt", "w")
 
-f_dev=open("DEV.txt", "r")
-dev_lines = f_dev.readlines()
+    f_dev=open("DEV.txt", "r")
+    dev_lines = f_dev.readlines()
 
-for l in dev_lines:
-    split = l.split(' ',1)
-    f_labels.write(split[0]+'\n')
-    f_questions.write(split[1])
+    for l in dev_lines:
+        split = l.split(' ',1)
+        split_label = split[0].split(':', 1)
 
-f_dev.close()
-f_labels.close()
-f_questions.close()
+        f_questions.write(split[1])
 
+        if model == '-coarse':
+            f_labels.write(split_label[0]+'\n')
+        else:
+            f_labels.write(split[0]+'\n')
 
-print ('Number of arguments:', len(sys.argv), 'arguments.')
-print ('Argument List:', str(sys.argv))
+    f_dev.close()
+    f_labels.close()
+    f_questions.close()
+
+#begginig
 
 arg_list = sys.argv
+model = arg_list[1]
+
+split_dev(model)
 
 f_train = open(arg_list[2], "r")
 f_dev_questions = open(arg_list[3], "r")
-f_dev_labels = open(arg_list[4], "r")
-
-model = arg_list[1]
 
 train_lines = f_train.readlines()
 dev_questions_lines = f_dev_questions.readlines()
-dev_labels_lines = f_dev_labels.readlines()
 
 train_questions = []
 train_labels = []
 
 dev_questions = []
-dev_labels = []
 
+#split questions and labels into different lists from TRAIN (considering the model)
 for l in train_lines:
     split = l.split(' ',1)
     split_label = split[0].split(':', 1)
@@ -58,23 +61,14 @@ for l in train_lines:
     else:
         train_labels.append(split[0])
 
+#put question from DEV in a list
 for l in dev_questions_lines:
     dev_questions.append(l)
 
-for l in dev_labels_lines:
-    split_label = l.split(':', 1)
-    if model == '-coarse':
-        dev_labels.append(split_label[0])
-    else:
-        dev_labels.append(l.replace('\n', ''))
-
 f_train.close()
 f_dev_questions.close()
-f_dev_labels.close()
-#pre processing
-print(dev_labels)
 
-# TODO: stemming
+#pre processing
 
 # Interface lemma tokenizer from nltk with sklearn
 class LemmaTokenizer:
@@ -89,6 +83,8 @@ def process (question):
     question = question.replace('\n', '')
     return question
 
+
+#apply preprocessing to all questions both from TRAIN and DEV
 train_questions = list(map(process, train_questions))
 dev_questions = list(map(process, dev_questions))
 
@@ -99,19 +95,19 @@ new_stop_words = stop_words.difference(exclude_words)
 tokenizer=LemmaTokenizer()
 token_stop = tokenizer(' '.join(new_stop_words))
 
-tfidfvectorizer = TfidfVectorizer(analyzer='word',stop_words= new_stop_words)#, tokenizer= tokenizer)
+tfidfvectorizer = TfidfVectorizer(analyzer='word',stop_words= token_stop, tokenizer= tokenizer)
 
 train_data = tfidfvectorizer.fit_transform(train_questions)
 dev_data = tfidfvectorizer.transform(dev_questions)
 
+
 #train
-clf = ExtraTreesClassifier(n_estimators=100, random_state=0)
+clf =  svm.SVC(kernel='linear')
 clf.fit(train_data, train_labels)
 
 #predict
 dev_predict = clf.predict(dev_data)
 
 
- #evaluating
-
-print(accuracy_score(dev_labels, dev_predict))
+for i in dev_predict:
+    print(i)
